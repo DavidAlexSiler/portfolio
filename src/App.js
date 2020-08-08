5d375d40f2e02c7e484098c535e999ab6ef30a5e
github username <github email address>
2020-08-07 23:57:19 -0700

    outline

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is Alex's Portfolio</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be the contents of my portfolio
         </p>
+        <li>Title</li>
+        <li>Picture LOOK NICE</li>
+        <li>Credentials or Info</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Links</li>
         <a
           className="App-link"
           href="https://reactjs.org"
