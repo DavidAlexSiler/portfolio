a5c07f10f0084ceed10c2439900f5b5d043f3043
github username <github email address>
2020-09-15 16:31:11 -0700

    cleaning out project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Nav />
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>This is Alex's Portfolio</h1>
         <p>
           This will be the contents of my portfolio
@@ -20,14 +15,6 @@ function App() {
         <li>About me</li>
         <li>Projects</li>
         <li>Links</li>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          poop your brain
-        </a>
       </header>
     </div>
   );
